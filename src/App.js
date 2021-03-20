625a7274c703bdb49f9fc1f83775976e62ef6339
Fariza Rafa <farizarafa123@gmail.com>
2021-03-20 22:58:31 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Login from './components/Login/Login';
+
+import Error from './components/Error/Error';
+import Destination from './components/Destination/Destination';
+import { createContext, useState } from 'react';
+import PrivateRoute from './components/Home/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
+
 
 function App() {
+  const [loggedInUser, setLoggedInUser ] = useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value ={[loggedInUser, setLoggedInUser]}>
+    <Router>
+      <Switch>
+
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+
+        <PrivateRoute path="/destination">
+          <Destination></Destination>
+        </PrivateRoute>
+       
+        <PrivateRoute path="/type/:type">
+        <Destination></Destination>
+        </PrivateRoute>
+        
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+
+        <Route path="*">
+          <Error></Error>
+        </Route>
+
+      </Switch>
+    </Router>
+    </UserContext.Provider>
   );
 }
 
